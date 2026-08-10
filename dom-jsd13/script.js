75c37f1c0f8e10fcf5f5c6f0faf64e30f4e74ead
fon.suteara <fon.suteara@gmail.com>
2026-08-10 16:25:20 +0700

    Ex1

--- dom-jsd13/script.js
+++ dom-jsd13/script.js
@@ -16,6 +16,16 @@ console.log(document.getElementById('card').innerHTML);
 const newCard = document.getElementById('card');
 newCard.innerHTML = "<img src='https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg'>";
 // 3. classList            -> #themeBtn, .card
+
 // 4. Create & Remove      -> #addTaskBtn, #resetTasksBtn, #tasks
 // 5. Events               -> #click-me, #list, #signupForm, #email, .error
+
+const btn = document.querySelector("#click-me");
+let count = 0;
+const eventDiv = document.querySelector("#event-div");
+btn.addEventListener('click', function(event) {
+    eventDiv.innerHTML += `<img width="200px" height="200px" src="https://i.etsystatic.com/20553578/r/il/70f1ba/3258604881/il_570xN.3258604881_7i19.jpg">`;
+})
 // 6. Pokémon Card Fetcher -> #fetchBtn, #resetBtn, #gallery
+const cardImg = document.getElementById("card");
+cardImg.innerHTML = `<img src = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png">`;
